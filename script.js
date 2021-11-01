98b599c1e98900258ed152168c1f39af717182c9
UK-HarryK <harrykilbride7@gmail.com>
2021-11-01 14:38:23 +0000

    made divs random colours

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 "use strict"
 
-alert("hello")
+function split4(){
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+}
+
+function rbgGenerator(){
+    let r = Math.floor(Math.random()*255)
+    let g = Math.floor(Math.random()*255)
+    let b = Math.floor(Math.random()*255)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+
+function addQuarter(intoDiv){
+    let q = document.createElement("div")
+    q.classList.add("quarter")
+    q.classList.add("flexy")
+    q.style.backgroundColor = rbgGenerator()
+    q.addEventListener("click", split4)
+    intoDiv.appendChild(q)
+}
+
+addQuarter(document.body)
+addQuarter(document.body)
+addQuarter(document.body)
+addQuarter(document.body)
